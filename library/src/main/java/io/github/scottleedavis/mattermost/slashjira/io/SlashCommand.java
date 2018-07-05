932e3747c18cd157425a19c1d3a62161c7f7988d
Scott Davis <scottd@collegenet.com>
2018-07-05 01:20:28 -0700

    more sync

--- library/src/main/java/io/github/scottleedavis/mattermost/slashjira/io/SlashCommand.java
+++ library/src/main/java/io/github/scottleedavis/mattermost/slashjira/io/SlashCommand.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController(value = "JiraSlashCommand")
 public class SlashCommand {
 
     @Value("${jira.slashCommandToken}")
